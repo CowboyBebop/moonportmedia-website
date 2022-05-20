addedca58acb1208b567df36c2ad1adfe8c819a9
DESKTOP-SSR8T9E\emir_ <emir_akbashev@mail.ru>
2022-05-20 17:00:06 +0300

    build test

--- content/testimonials.ts
+++ content/testimonials.ts
@@ -4,7 +4,7 @@ const testimonials = [
     subscribers: '140,284',
     image:
       'https://yt3.ggpht.com/JOO_l1HkJ9qDi1wkbkwA-xVgrr5E1vF1en3mq6j-7hmk-AuEAd4ziEjpWKHlOmOJwbJbfk7L=s88-c-k-c0x00ffffff-no-rj',
-    text: "Moonport helped me to take the YouTube channel to the next level and have been a huge convenience to keep everything rolling smoothly",
+    text: "Moonport helped me to take the YouTube channel to the next level and have been a huge convenience to keep everything rolling smoothly.",
     link: 'https://www.youtube.com/ottryt',
   },
 ]
