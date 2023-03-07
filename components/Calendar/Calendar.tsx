f4f9e5f97b6c90cdb1e211ab02047418b61d13e5
Cowboy Bebop <33606633+CowboyBebop@users.noreply.github.com>
2023-03-07 20:18:52 +0200

    fixing grid again

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -28,7 +28,7 @@ const Calendar: React.FC = () => {
         <Arrow />
       </S.Title>
       
-      <div style={{display: 'flex', justify-content:"center"}}>
+      <div style={{display: 'flex', justify-content:'center'}}>
 
         <motion.div variants={Fade}>
           {dark ? (
