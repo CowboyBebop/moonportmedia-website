0b1b3670092358b73c77281717f24d653808c620
Cowboy Bebop <33606633+CowboyBebop@users.noreply.github.com>
2023-03-11 14:52:37 +0200

    updated testimonials

--- content/testimonials.ts
+++ content/testimonials.ts
@@ -1,12 +1,22 @@
 const testimonials = [
   {
     name: "Ottr",
-    subscribers: '140,284',
+    subscribers: '243,413',
     image:
       'https://yt3.ggpht.com/JOO_l1HkJ9qDi1wkbkwA-xVgrr5E1vF1en3mq6j-7hmk-AuEAd4ziEjpWKHlOmOJwbJbfk7L=s88-c-k-c0x00ffffff-no-rj',
     text: "Moonport helped me to take the YouTube channel to the next level and have been a huge convenience to keep everything rolling smoothly.",
-    link: 'https://www.youtube.com/ottryt',
+    link: 'https://www.youtube.com/@ottr',
   },
+/*
+  {
+    name: "Extesyy",
+    subscribers: '310,426',
+    image:
+      'https://yt3.googleusercontent.com/ytc/AL5GRJXcED5u595WrpdAVajlo0nkidqFiohF6joNFvIu=s176-c-k-c0x00ffffff-no-rj',
+    text: "Moonport helped me to take the YouTube channel to the next level and have been a huge convenience to keep everything rolling smoothly.",
+    link: 'https://www.youtube.com/@extesyy2010',
+  },
+*/
 ]
 
 export default testimonials
